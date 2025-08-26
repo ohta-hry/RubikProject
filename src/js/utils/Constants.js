12963d15449b1d53dd0476bf1bf30f38ec4c2e43
ohta-hry <haruyoshitwonine@gmail.com>
2025-08-26 17:15:33 +0900

    rubikOperations　いったん仮完成

--- src/js/utils/Constants.js
+++ src/js/utils/Constants.js
@@ -1,3 +1,5 @@
+import * as THREE from 'three';
+
 export const CUBE_COLORS = {
     front: 0xFFFFFF,   // 白（前面）
     back:  0xFFDD00,   // 明るい黄（後面）
@@ -13,4 +15,19 @@ export const CUBE_TYPES = {
     EDGE: 'edge',
     CENTER: 'center',
     CORE: 'core'
+};
+
+
+// 90度単位のクォータニオン定数を定義
+export const QUA_ANGLE = {
+    // 単軸回転
+    X90: new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(1, 0, 0), -Math.PI / 2),
+    Xm90: new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(1, 0, 0), Math.PI / 2),
+    Y90: new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), -Math.PI / 2),
+    Ym90: new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI / 2),
+    Z90: new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 0, 1), -Math.PI / 2),
+    Zm90: new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 0, 1), Math.PI / 2),
+    
+    // 単位クォータニオン（何も回転しない）
+    identity: new THREE.Quaternion()
 };
